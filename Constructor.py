412572370999f117349dc9d5bb9b3f0625a2a4ae
Mmesomachi <94153994+MathsMarshall@users.noreply.github.com>
2024-04-20 20:47:52 +0300

    Update Constructor.py

--- Constructor.py
+++ Constructor.py
@@ -1,12 +1,12 @@
 import copy
-class cuboid1x1x2():
-    def __init__(self, coordinate, grid):
+class cuboid1x1x2full():
+    def __init__(self, coordinate: tuple[tuple], grid: str):
         self.coordinate = coordinate # tuple of tuples
         self.path = '' # string of direction
         self.recall = '' # string of reverse direction
         self.grid = grid # set of tuples
     @property
-    def state(self):
+    def state(self) -> str:
         c = self.coordinate
         if len(c) == 1:
             return '1x1'
@@ -15,11 +15,11 @@ class cuboid1x1x2():
         else: # c[0][0] == c[1][0]
             return '1x2'
     @property
-    def is_in_bound(self): # grid is a set of tuples
+    def is_in_bound(self) -> bool: # grid is a set of tuples
         return (set(self.coordinate).issubset(self.grid))
     
     @staticmethod
-    def opposite(direction):
+    def opposite(direction: str) -> str:
         if (direction == 'U'):
             return 'D'
         elif (direction == 'D'):
@@ -29,7 +29,105 @@ class cuboid1x1x2():
         else:# (direction == 'R'):
             return 'L'
     
-    def roll(self, direction): # direction is a string from U, D, L, R
+    def not_reverse(self, direction: str) -> bool:
+        if self.path == '':
+            return True
+        elif (self.path[-1] == 'U') and (direction == 'D'):
+            return False
+        elif (self.path[-1] == 'D') and (direction == 'U'):
+            return False
+        elif (self.path[-1] == 'L') and (direction == 'R'):
+            return False
+        elif (self.path[-1] == 'R') and (direction == 'L'):
+            return False
+        else:
+            return True
+    
+    def is_next_in_bound(self, direction: str) -> bool:
+        cube = copy.copy(self)
+        cube.roll(direction)
+        return cube.is_in_bound
+
+    def go(self, direction: str) -> bool:
+        return self.is_next_in_bound(direction) and self.not_reverse(direction)
+
+
+    def forward(self, my_dict: dict, my_dict1: dict, end: tuple[tuple]) -> tuple[dict, dict, dict, set]:
+        '''Updates set of possible paths. paths is a 
+        list of the possible .path attributes. coordinates is 
+        a list of coordinates occupied. path in paths are the keys
+        for coordinate in coordinates for dictionary my_dict,
+        my_dict1 has corresponding keys with my_dict, but value is
+        current coordinate as tuple'''
+        my_dict_ = {}
+        my_dict1_ = {}
+        check = {}
+        checkset = set()
+        Directions = ['U', 'D', 'L', 'R']
+        for path, coordinate in my_dict1.items(): # Here coordinate is a tuple of tuples
+            coordinates = my_dict[path]
+            for direction in Directions:
+                a = cuboid1x1x2full(coordinate, self.grid)
+                a.path = path
+                if a.go(direction):
+                    a.roll(direction) # Perform roll
+                    if a.coordinate not in my_dict[path]: 
+                        ss = copy.deepcopy(my_dict[path])
+                        ss.add(a.coordinate)
+                        my_dict_[a.path] = ss # Update list of coordinates occupied for paths
+                        my_dict1_[a.path] = a.coordinate # Update current coordinate occupied with respect to path
+                        check[a.coordinate] = a.path #  Inverse dictionary for my_dict1_
+                        checkset.add(a.coordinate) # Set of coordinates occupied
+        return my_dict_, my_dict1_, check, checkset
+
+    def backward(self, my_dict: dict, my_dict1: dict, start: tuple[tuple]) -> tuple[dict, dict, dict, set]:
+        '''Updates set of possible recalls. recalls is a 
+        list of the possible .recall attributes. coordinates is 
+        a list of coordinates occupied. recall in recalls are the keys
+        for coordinate in coordinates for dictionary my_dict,
+        my_dict1 has corresponding keys with my_dict, but value is
+        current coordinate as tuple'''
+        my_dict_ = {}
+        my_dict1_ = {}
+        check = {}
+        checkset = set()
+        Directions = ['U', 'D', 'L', 'R']
+        for recall, coordinate in my_dict1.items(): # Here coordinate is a tuple of tuples
+            coordinates = my_dict[recall]
+            for direction in Directions:
+                a = cuboid1x1x2full(coordinate, self.grid)
+                a.recall = recall
+                if a.go(direction):
+                    a.roll(direction) # Perform roll
+                    if a.coordinate not in my_dict[recall]: 
+                        ss = copy.deepcopy(my_dict[recall])
+                        ss.add(a.coordinate)
+                        my_dict_[a.recall] = ss # Update list of coordinates occupied for recalls
+                        my_dict1_[a.recall] = a.coordinate # Update current coordinate occupied with respect to recall
+                        check[a.coordinate] = a.recall # Inverse dictionary for my_dict1_
+                        checkset.add(a.coordinate) # Set of coordinates occupied
+        return my_dict_, my_dict1_, check, checkset
+
+    def solver(self, start: tuple[tuple], end: tuple[tuple]) -> str:
+        cuboid = cuboid1x1x2full(start, self.grid) # Create cuboid
+        my_dict = {'': {tuple(start)}}
+        my_dict1 = {'': tuple(start)}
+        my_dict_ = {'': {tuple(end)}}
+        my_dict1_ = {'': tuple(end)}
+        my_dict, my_dict1, check, checkset = self.forward(my_dict, my_dict1, end)
+        while True:
+            my_dict_, my_dict1_, check_, checkset_ = self.backward(my_dict_, my_dict1_, start)
+            if checkset & checkset_:
+                s = list(checkset & checkset_)[0]
+                s1 = check[s] + check_[s]
+                return s1
+            my_dict, my_dict1, check, checkset = self.forward(my_dict, my_dict1, end)
+            if checkset & checkset_:
+                s = list(checkset & checkset_)[0]
+                s1 = check[s] + check_[s]
+                return s1
+    
+    def roll(self, direction: str) -> None: # direction is a string from U, D, L, R
         self.path = self.path + direction
         self.recall = self.opposite(direction) + self.recall
         c = self.coordinate # tuple
